59f753ede1db9e2f2b5b8d6bbeaa0b226b1b053c
Nicolas DEROO <deroo.nicola@gmail.com>
2023-12-22 17:36:55 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PanierComponent } from './panier/panier.component';
+import { ListeProduitsComponent } from './produits/liste-produits/liste-produits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'panier',
+    component: PanierComponent,
+  },
+  {
+    path: 'catalogue',
+    component: ListeProduitsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
